e232ec3e25c302760e967afcbef4f2b2413fe347
xbony2 <xbony22@gmail.com>
2015-08-11 12:44:18 -0400

    I'm learning C++ as I go :P

--- src/Navbox Converter.cpp
+++ src/Navbox Converter.cpp
@@ -4,11 +4,18 @@
 
 using namespace std;
 
-int main() {
-	string navbox;
+int main(){
+	string navbox [1000000];
 	ifstream infile("navbox");
-	infile >> navbox;
-	cout << navbox << endl;
+	int count;
+
+	infile >> count;
+	for(int i = 0; i < count; i++){
+		infile >> navbox[i];
+		cout << navbox[i] << endl;
+	}
+
+	//cout << navbox << endl;
 	infile.close();
 
 	return 0;
