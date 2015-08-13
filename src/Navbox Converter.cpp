d63897daccde386c280f4b13ff8edbe7eae05238
xbony2 <xbony22@gmail.com>
2015-08-13 11:08:49 -0400

    upgraded C++ version to C++1y/C++14 (it isn't 1999 anymore :P)

--- src/Navbox Converter.cpp
+++ src/Navbox Converter.cpp
@@ -102,8 +102,7 @@ public:
 };
 
 int main(){
-	vector<int> v(3);
-	v = {1, 5, 7};
+	vector<int> v = { 34,23 };
 	Navbox navbox = new Navbox("{{L|Santa's Toys}}", "Navbox Santa's Toys", new vector<Group>{
 		{"Items", new vector<NavboxThing>{{NavboxThing::ITEM, "STS", "Match"}, {NavboxThing::ITEM, "STS", "Ender Blaster"}, {NavboxThing::ITEM, "STS", "Nether Blaster"}}},
 		{"Blocks", new vector<NavboxThing>{{NavboxThing::ITEM, "STS", "Block of Damn Giving"}, {NavboxThing::ITEM, "V", "Glowstone", "Heavy Light"}, {NavboxThing::ITEM, "STS", "Spiced Sand"}}}});
